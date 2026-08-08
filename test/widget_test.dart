5e42bc61d98c927d774a097f8dd478fd88d5e3ac
Lajgit <3243570032@qq.com>
2026-08-08 16:15:54 +0800

    test(app): keep generated widget test compatible

---
+++ test/widget_test.dart
@@ -0,0 +1,11 @@
+import 'package:diandi_memory/app/app.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('启动后显示微信档案导入入口', (tester) async {
+    await tester.pumpWidget(const DiandiMemoryApp());
+
+    expect(find.text('导入微信聊天档案'), findsOneWidget);
+    expect(find.text('选择 ZIP 文件'), findsOneWidget);
+  });
+}

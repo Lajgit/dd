1e7a648ac6ea331f3966bf3c71a752384a1298e9
Lajgit <3243570032@qq.com>
2026-08-08 18:07:52 +0800

    test: cover warm home navigation

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,10 +2,12 @@ import 'package:diandi_memory/app/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('启动后显示微信档案导入入口', (tester) async {
+  testWidgets('启动后显示微信档案导入入口和回忆导航', (tester) async {
     await tester.pumpWidget(const DiandiMemoryApp());
 
     expect(find.text('导入微信聊天档案'), findsOneWidget);
     expect(find.text('选择 ZIP 文件'), findsOneWidget);
+    expect(find.text('回忆'), findsOneWidget);
+    expect(find.text('导入'), findsOneWidget);
   });
 }
